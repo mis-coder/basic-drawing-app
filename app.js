ac5493c6f497ea175f22b3699c6648c21754d24d
Antra Verma <antraverma99@gmail.com>
2022-03-30 18:33:18 +0530

    added basic drawing area

--- app.js
+++ app.js
@@ -1,2 +1,47 @@
-const drawingArea = document.querySelector("#drawing-area");
-const context = drawingArea.getContext("2d");
+const canvas = document.getElementById("drawing-area");
+const ctx = canvas.getContext("2d");
+
+let isDrawing = false;
+
+const startDrawing = () => {
+  isDrawing = true;
+};
+
+const endDrawing = () => {
+  isDrawing = false;
+  ctx.beginPath();
+};
+
+const drawWithMouse = (event) => {
+  if (!isDrawing) return;
+
+  ctx.lineWidth = 9;
+  ctx.lineCap = "round";
+  ctx.strokeStyle = "red";
+  ctx.lineTo(
+    event.clientX - canvas.offsetLeft,
+    event.clientY - canvas.offsetTop
+  );
+  ctx.stroke();
+};
+
+const drawWithTouch = (event) => {
+  if (!isDrawing) return;
+
+  ctx.lineWidth = 9;
+  ctx.lineCap = "round";
+  ctx.strokeStyle = "red";
+  ctx.lineTo(
+    event.touches[0].clientX - canvas.offsetLeft,
+    event.touches[0].clientY - canvas.offsetTop
+  );
+  ctx.stroke();
+};
+
+canvas.addEventListener("mousedown", startDrawing);
+canvas.addEventListener("mousemove", drawWithMouse);
+canvas.addEventListener("mouseup", endDrawing);
+
+canvas.addEventListener("touchstart", startDrawing);
+canvas.addEventListener("touchmove", drawWithTouch);
+canvas.addEventListener("touchend", endDrawing);
