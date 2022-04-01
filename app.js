0a67d2911db83c268971347a604bbc3add5d69cb
Antra Verma <antraverma99@gmail.com>
2022-04-01 18:18:40 +0530

    added tools

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const canvas = document.getElementById("drawing-area");
+const colorPicker = document.querySelector("#color-picker");
+const thicknessSlider = document.querySelector("#thickness-slider");
+const undoBtn = document.querySelector("#undo-btn");
+const clearBtn = document.querySelector("#clear-btn");
+const downloadBtn = document.querySelector("#download-btn");
+const canvas = document.querySelector("#drawing-area");
 const ctx = canvas.getContext("2d");
 
 let isDrawing = false;
+const strokePaths = [];
+const restoreStrokePaths = [];
+let index = -1;
+
+let penColor = "#000";
+let penThickness = 5;
 
 const startDrawing = () => {
   isDrawing = true;
@@ -9,15 +20,22 @@ const startDrawing = () => {
 
 const endDrawing = () => {
   isDrawing = false;
+  strokePaths.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
+  index += 1;
   ctx.beginPath();
+};
+
+const setPenStyle = (styles) => {
+  const { thickness, color, capStyle } = styles;
+  ctx.lineWidth = thickness;
+  ctx.lineCap = capStyle;
+  ctx.strokeStyle = color;
 };
 
 const drawWithMouse = (event) => {
   if (!isDrawing) return;
 
-  ctx.lineWidth = 9;
-  ctx.lineCap = "round";
-  ctx.strokeStyle = "red";
+  setPenStyle({ thickness: penThickness, color: penColor, capStyle: "round" });
   ctx.lineTo(
     event.clientX - canvas.offsetLeft,
     event.clientY - canvas.offsetTop
@@ -28,14 +46,44 @@ const drawWithMouse = (event) => {
 const drawWithTouch = (event) => {
   if (!isDrawing) return;
 
-  ctx.lineWidth = 9;
-  ctx.lineCap = "round";
-  ctx.strokeStyle = "red";
+  setPenStyle({ thickness: penThickness, color: penColor, capStyle: "round" });
   ctx.lineTo(
     event.touches[0].clientX - canvas.offsetLeft,
     event.touches[0].clientY - canvas.offsetTop
   );
   ctx.stroke();
+};
+
+const setPenColor = (event) => {
+  penColor = event.target.value;
+};
+
+//TODO: in progress
+const undoYourMove = () => {
+  if (index <= 0) {
+    clearDrawingArea();
+    return;
+  }
+  const path = strokePaths.pop();
+  restoreStrokePaths.push(path);
+  console.log(restoreStrokePaths);
+  index -= 1;
+  ctx.putImageData(strokePaths[index], 0, 0);
+};
+
+//TODO
+const redoYourMove = () => {};
+
+const clearDrawingArea = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+const downloadDrawingImage = (event) => {
+  event.target.href = canvas.toDataURL();
+};
+
+const setPenThickness = (event) => {
+  penThickness = event.target.value;
 };
 
 canvas.addEventListener("mousedown", startDrawing);
@@ -45,3 +93,9 @@ canvas.addEventListener("mouseup", endDrawing);
 canvas.addEventListener("touchstart", startDrawing);
 canvas.addEventListener("touchmove", drawWithTouch);
 canvas.addEventListener("touchend", endDrawing);
+
+colorPicker.addEventListener("change", setPenColor);
+thicknessSlider.addEventListener("change", setPenThickness);
+downloadBtn.addEventListener("click", downloadDrawingImage);
+clearBtn.addEventListener("click", clearDrawingArea);
+undoBtn.addEventListener("click", undoYourMove);
