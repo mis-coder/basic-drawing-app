85268129491a9bd03d634bd59ab20d85ef278f7b
Antra Verma <antraverma99@gmail.com>
2022-04-02 16:18:28 +0530

    added styles

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
+//TODO: refactor code
+//TODO: disable undo/redo functionality
 const colorPicker = document.querySelector("#color-picker");
 const thicknessSlider = document.querySelector("#thickness-slider");
 const undoBtn = document.querySelector("#undo-btn");
+const redoBtn = document.querySelector("#redo-btn");
 const clearBtn = document.querySelector("#clear-btn");
 const downloadBtn = document.querySelector("#download-btn");
 const canvas = document.querySelector("#drawing-area");
 const ctx = canvas.getContext("2d");
 
+canvas.height = window.innerHeight - 64;
+canvas.width = window.innerWidth;
 let isDrawing = false;
 const strokePaths = [];
 const restoreStrokePaths = [];
 let index = -1;
+let rIndex = -1;
 
-let penColor = "#000";
+let penColor = "#DE3163";
 let penThickness = 5;
-
+ctx.font = "30px Arial";
+ctx.textAlign = "center";
+ctx.fillText(
+  "start moving cursor on the white area!",
+  canvas.width / 2,
+  canvas.height / 2
+);
 const startDrawing = () => {
+  if (strokePaths.length === 0) {
+    clearDrawingArea();
+  }
   isDrawing = true;
 };
 
@@ -55,7 +70,9 @@ const drawWithTouch = (event) => {
 };
 
 const setPenColor = (event) => {
-  penColor = event.target.value;
+  const color = event.target.value;
+  penColor = color;
+  penColor.style.backgroundColor = color;
 };
 
 //TODO: in progress
@@ -65,14 +82,27 @@ const undoYourMove = () => {
     return;
   }
   const path = strokePaths.pop();
-  restoreStrokePaths.push(path);
-  console.log(restoreStrokePaths);
+  restoreStrokePaths.unshift(path);
+  rIndex += 1;
   index -= 1;
   ctx.putImageData(strokePaths[index], 0, 0);
+  console.log("U stroke: ", strokePaths);
+  console.log("U restore: ", restoreStrokePaths);
 };
 
 //TODO
-const redoYourMove = () => {};
+const redoYourMove = () => {
+  if (rIndex === -1) {
+    return;
+  }
+  ctx.putImageData(restoreStrokePaths.at(-1), 0, 0);
+  const path = restoreStrokePaths.pop();
+  strokePaths.push(path);
+  index += 1;
+  rIndex -= 1;
+  console.log("R stroke: ", strokePaths);
+  console.log("R restore: ", restoreStrokePaths);
+};
 
 const clearDrawingArea = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -99,3 +129,4 @@ thicknessSlider.addEventListener("change", setPenThickness);
 downloadBtn.addEventListener("click", downloadDrawingImage);
 clearBtn.addEventListener("click", clearDrawingArea);
 undoBtn.addEventListener("click", undoYourMove);
+redoBtn.addEventListener("click", redoYourMove);
